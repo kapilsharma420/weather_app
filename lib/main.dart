ee38c2f5a6dd26d623b6887468941bc367646a23
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-04-04 17:53:20 +0530

    add packege and homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:whether_app/screen/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'weather app',
+      home: Homescreen(),
+    );
+  }
+}
