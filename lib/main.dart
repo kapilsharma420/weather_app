430bb27e6f8358bdc0a31367346f29b174c2946d
KAPIL SHARMA <140537615+kapilsharma420@users.noreply.github.com>
2025-04-05 11:25:26 +0530

    weather app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:whether_app/screen/homescreen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:whether_app/screen/weather_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'weather app',
-      home: Homescreen(),
+      title: 'Weather App',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.blueAccent,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: WeatherHomePage(),
     );
   }
 }
+
